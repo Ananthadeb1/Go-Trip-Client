766d3a7b5b4323cc01b46894b3ab6666f76e7ed5
Anantha Debnath <ananthadebnath103@gmail.com>
2025-04-23 00:08:37 +0600

    some modification

--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -4,20 +4,19 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 const Footer = () => {
     return (
         <footer>
-            <div className="footer flex gap-0 py-0 text-white">
-                <div className="w-1/2 py-10 flex justify-center items-center" style={{ backgroundColor: '#1F2937' }}>
+            <div className="footer h-72 flex flex-col md:flex-row gap-0 py-0 text-white">
+                <div className="w-full h-full md:w-1/2  flex justify-center items-center" style={{ backgroundColor: '#1F2937' }}>
                     <div className="text-center h-max">
-                        <div className="text-center"></div>
                         <h2 className="text-2xl font-bold mb-4">Contact Us</h2>
                         <p className="mb-2">Email: contact@codecrafters.com</p>
                         <p className="mb-2">Phone: +1 (123) 456-7890</p>
                         <p className="mb-2">Address: 123 Code Street, Dev City, 45678</p>
                     </div>
                 </div>
-                <div className="w-1/2 flex justify-center items-center" style={{ backgroundColor: '#111827' }}>
-                    <div className="text-center h-max py-13.5">
+                <div className="w-full md:w-1/2 h-full flex  justify-center items-center" style={{ backgroundColor: '#111827' }}>
+                    <div className="text-center">
                         <h2 className="text-2xl font-bold mb-4">Follow Us</h2>
-                        <p>join us in socials</p>
+                        <p>Join us on socials</p>
                         <div className="flex justify-center gap-4 mt-4">
                             <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
                                 <FontAwesomeIcon icon={faFacebookF} size="lg" />
@@ -35,12 +34,12 @@ const Footer = () => {
                     </div>
                 </div>
             </div>
-            <div className="p-2 footer-cente text-white text-center" style={{ backgroundColor: '#151515' }}>
+            <div className="p-2 footer-center text-white text-center" style={{ backgroundColor: '#151515' }}>
                 <div>
                     <p>Copyright © 2025 - All right reserved by Code_Crafters</p>
                 </div>
             </div>
-        </footer >
+        </footer>
     );
 };
 
