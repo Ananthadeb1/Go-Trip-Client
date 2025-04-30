3667dcfc4896a5f8cde70064eef845b18b4b2604
Anantha Debnath <ananthadebnath103@gmail.com>
2025-04-30 16:26:16 +0600

    useAdmin problem solved

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -3,33 +3,36 @@ import useAuth from './useAuth';
 import { useNavigate } from 'react-router-dom';
 
 const axiosSecure = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'http://localhost:5000',
 })
+axiosSecure.interceptors.request.use(function (config) {
+    const token = localStorage.getItem('access-token');
+    // console.log("request stopped by config", token);
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+}, function (error) {
+    // do something with request error
+    return Promise.reject(error);
+});
+
+axiosSecure.interceptors.response.use(function (response) {
+    return response;
+}, async (error) => {
+    const status = error.response?.status;
+    // console.log("interceptor error", status);
+    if (status === 401 || status === 403) {
+        const { logout } = useAuth();
+        const navigate = useNavigate();
+        await logout();
+        navigate('/login');
+        console.log("interceptor error", status);
+    }
+    return Promise.reject(error);
+});
+
 const useAxiosSecure = () => {
-    const navigate = useNavigate();
-    const { logout } = useAuth();
-    axiosSecure.interceptors.request.use(function (config) {
-        const token = localStorage.getItem('access-token');
-        // console.log("request stoped by config", token);
-        if (token) {
-            config.headers.authorization = `Bearer ${token}`;
-        }
-        return config;
-    }, function (error) {
-        // do something with request error
-        return Promise.reject(error);
-    });
-    axiosSecure.interceptors.response.use(function (response) {
-        return response;
-    }, async (error) => {
-        const status = error.response.status;
-        // console.log("interceptor error", status);
-        if (status === 401 || status === 403) {
-            await logout();
-            navigate('/login');
-        }
-        return Promise.reject(error);
-    })
     return axiosSecure;
 };
 export default useAxiosSecure;
